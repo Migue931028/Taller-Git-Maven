40007aa1602c00eeaa533e7ee2ada1dcd5a4488d
Migue931028 <miguelangel.931028@gmail.com>
2026-08-20 17:20:07 -0500

    feat: agregar saludo personalizado

--- src/ui/java/Main.java
+++ src/ui/java/Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println("Hello please give me your name:");
+        String name= sc.nextLine();
+        System.out.println("Hello "+ name +" have a good day" );
     }
 }
